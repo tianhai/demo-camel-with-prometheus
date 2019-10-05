5fe0bb2c028e9c6a000a487587ee931644d613b0
jacky <jacky.chen@nzqa.govt.nz>
2019-10-05 18:06:03 +1300

    initial code commit

---
+++ src/main/java/nz/co/transpower/es/integration/democamelwithprometheus/DemoCamelWithPrometheusApplication.java
@@ -0,0 +1,13 @@
+package nz.co.transpower.es.integration.democamelwithprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCamelWithPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCamelWithPrometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/co/transpower/es/integration/democamelwithprometheus/DemoCamelWithPrometheusApplicationTests.java
@@ -0,0 +1,16 @@
+package nz.co.transpower.es.integration.democamelwithprometheus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoCamelWithPrometheusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
